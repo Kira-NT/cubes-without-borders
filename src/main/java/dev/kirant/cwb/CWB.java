7f06e8a3fe88e126061846530654971987e205c9
Kira-NT <kira.canary@proton.me>
2026-04-11 22:28:02 +0100

    Define mod's entry point

---
+++ src/main/java/dev/kirant/cwb/CWB.java
@@ -0,0 +1,9 @@
+package dev.kirant.cwb;
+
+public final class CWB {
+    public static final String MOD_ID = "cwb";
+
+    public static final CWBConfig CONFIG = CWBConfig.loadById(CWB.MOD_ID);
+
+    private CWB() { }
+}
