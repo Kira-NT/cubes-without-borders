f57c6b16bfba9879187008b7843904ffc745e048
Kira-NT <kira.canary@proton.me>
2026-04-11 18:33:26 +0100

    Add `FullscreenMode` to represent fullscreen modes

---
+++ src/main/java/dev/kirant/cwb/FullscreenMode.java
@@ -0,0 +1,38 @@
+package dev.kirant.cwb;
+
+public enum FullscreenMode {
+    OFF(0, "options.off"),
+    ON(1, "options.on"),
+    BORDERLESS(2, "cwb.options.borderless");
+
+    private final int id;
+
+    private final String translationKey;
+
+    FullscreenMode(int id, String translationKey) {
+        this.id = id;
+        this.translationKey = translationKey;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTranslationKey() {
+        return this.translationKey;
+    }
+
+    public static FullscreenMode get(int id) {
+        id = ((id % 3) + 3) % 3;
+
+        if (id == ON.id) {
+            return ON;
+        }
+
+        if (id == BORDERLESS.id) {
+            return BORDERLESS;
+        }
+
+        return OFF;
+    }
+}
